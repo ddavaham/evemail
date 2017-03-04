90257d2b829b63de469c2ca6760d8b60d216a1e1
David Douglas <ddouglas@douglaswebdev.net>
2017-03-04 04:35:24 +0000

    Commit new email migrations and token managing changes

---
+++ app/UserEmail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EVEMail;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserEmail extends Model
+{
+    protected $primaryKey = 'character_id';
+    protected $table = 'user_emails';
+    protected $fillable = ['character_id', 'character_email', 'email_opt_in', 'email_verification_code'];
+}
---
+++ database/migrations/2017_02_28_171004_create_user_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_emails', function (Blueprint $table) {
+            $table->bigInteger('character_id');
+            $table->string('character_email');
+            $table->string('email_verification_code')->nullable();
+            $table->boolean('email_opt_in');
+            $table->timestamps();
+
+            $table->primary('character_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_emails');
+    }
+}
